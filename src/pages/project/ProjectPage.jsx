0b3a52911b198eb3f0ede164e8f7d75bae166da4
Tanesh <tanesh753@gmail.com>
2025-02-21 11:02:35 +0530

    resume updated

---
+++ src/pages/project/ProjectPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Fade  from "react-reveal/Fade";
+import Blast from "../BlastAnimation/Blast";
+
+
+const ProjectPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProjectPage
